7d547d13f1a607c1fefb4ef5c905ceebf52106f8
jkmlgh <jkmlgh>
2015-05-30 03:16:19 +0800

    Commit all existing static pages

---
+++ concerts/concerts.js
@@ -0,0 +1,16 @@
+function changePage(selectObj)
+{
+	var value = selectObj.options[selectObj.selectedIndex].value;
+	if (!value) {
+		value = selectObj.options[selectObj.selectedIndex].text;
+	}
+	window.location.href="concerts"+value+".html";
+}
+
+var opts = "<option>Please select...</option>";
+for (var i = 2015; i >= 1999; --i) {
+	opts += "<option>" + i + "</option>";
+}
+opts += '<option value="199x">Earlier</option>';
+
+document.write('<select onchange="changePage(this)">' + opts + '</select>');
